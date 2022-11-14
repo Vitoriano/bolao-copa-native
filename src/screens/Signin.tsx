fc3a3258f0d31dece3f057456242cedb96ed73a1
Vitoriano <vitorianoernandes2012@gmail.com>
2022-11-14 11:58:53 -0300

    Criando o component de Button

--- src/screens/Signin.tsx
+++ src/screens/Signin.tsx
@@ -1,12 +1,22 @@
 
-import { Center,Text} from "native-base";
+import { Center,Text, Icon} from "native-base";
+import { Fontisto } from '@expo/vector-icons';
 
 import Logo from '../assets/logo.svg';
+import { Button } from "../components/Button";
 
 export function Signin() {
   return(
     <Center flex={1} bgColor="gray.900" >
      <Logo width={212} height={40}/>
+
+     <Button 
+      title="Entra com o Google"
+      leftIcon={<Icon  as={Fontisto} name="google" color="white" size="md"/>}
+      type="SECONDARY"
+      
+     />
+
     </Center>
   );
 }
